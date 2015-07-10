5c77edff31a238cd93cd94817ba279e72b12293b
equalsFrontend <alex.boisselle@gmail.com>
2015-07-10 16:24:36 -0400

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,6 +34,7 @@ module.exports = function (grunt) {
     //
     //grunt.registerTask('default',   ['concat', 'karma:development', 'compass', 'cssmin', 'watch']);
     //grunt.registerTask('cleanall',  ['clean:all', 'copy:main']);
-    //grunt.registerTask('test', ['karma:development']);
+    
+    grunt.registerTask('test', ['karma:development']);
 
 };
