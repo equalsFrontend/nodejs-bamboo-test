7d7aaf0bd5986ff922370fd2117181ce8a51d7e1
equalsFrontend <alex.boisselle@gmail.com>
2015-07-13 15:11:37 -0400

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,8 @@
 module.exports = function(config) {
     config.set({
 
-        // base path that will be used to resolve all patterns (eg. files, exclude)
         basePath: '',
 
-        // frameworks to use
-        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
         frameworks: ['jasmine'],
 
         files: [
@@ -15,20 +12,14 @@ module.exports = function(config) {
             'test/**/*Src.js'
         ],
 
-        // list of files to exclude
         exclude: [
 
         ],
 
-        // preprocess matching files before serving them to the browser
-        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
 
         },
 
-        // test results reporter to use
-        // possible values: 'dots', 'progress'
-        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
         reporters: ['junit', 'spec'],
 
         junitReporter: {
@@ -36,25 +27,17 @@ module.exports = function(config) {
             suite: ''
         },
 
-        // web server port
         port: 9876,
 
-        // enable / disable colors in the output (reporters and logs)
         colors: true,
 
-        // level of logging
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
         logLevel: config.LOG_DEBUG,
 
-        // enable / disable watching file and executing tests whenever any file changes
         autoWatch: false,
 
-        // start these browsers
-        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         browsers: ['PhantomJS'],
 
-        // Continuous Integration mode
-        // if true, Karma captures browsers, runs the tests and exits
         singleRun: true
     });
 };
